a4d6cdae8b70f15e40380e51a3c82d9ca674a821
fadhil27 <fadhilkh27@gmail.com>
2021-11-11 23:37:01 +0800

    Use a ViewModelFactory

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2019 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
@@ -32,14 +16,11 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
 /**
  * Fragment where the game is played
  */
-
 class GameFragment : Fragment() {
 
     private lateinit var binding: GameFragmentBinding
 
-
     private lateinit var viewModel: GameViewModel
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -51,8 +32,9 @@ class GameFragment : Fragment() {
             container,
             false
         )
-
         Log.i("GameFragment", "Called ViewModelProvider.get")
+
+        // Get the viewModel
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         binding.correctButton.setOnClickListener { onCorrect() }
@@ -61,11 +43,9 @@ class GameFragment : Fragment() {
         updateScoreText()
         updateWordText()
         return binding.root
-
     }
 
-
-    /** Methods for button click handlers **/
+    /** Methods for buttons presses **/
 
     private fun onSkip() {
         viewModel.onSkip()
@@ -77,6 +57,7 @@ class GameFragment : Fragment() {
         updateScoreText()
         updateWordText()
     }
+
     private fun onEndGame() {
         gameFinished()
     }
@@ -85,12 +66,16 @@ class GameFragment : Fragment() {
 
     private fun updateWordText() {
         binding.wordText.text = viewModel.word
+
     }
 
     private fun updateScoreText() {
         binding.scoreText.text = viewModel.score.toString()
     }
 
+    /**
+     * Called when the game is finished
+     */
     private fun gameFinished() {
         Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
         val action = GameFragmentDirections.actionGameToScore()
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -3,15 +3,20 @@ package com.example.android.guesstheword.screens.game
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
+/**
+ * ViewModel containing all the logic needed to run the game
+ */
 class GameViewModel : ViewModel() {
-
 
     // The current word
     var word = ""
+
     // The current score
     var score = 0
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
+
 
     /**
      * Resets the list of words and randomizes the order
@@ -49,30 +54,31 @@ class GameViewModel : ViewModel() {
         nextWord()
     }
 
+    /**
+     * Callback called when the ViewModel is destroyed
+     */
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 
-
-    /** Methods for buttons presses **/
+    /** Methods for updating the UI **/
     fun onSkip() {
         score--
         nextWord()
     }
-
     fun onCorrect() {
         score++
         nextWord()
     }
 
+    /**
+     * Moves to the next word in the list.
+     */
     private fun nextWord() {
+        //Select and remove a word from the list
         if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
             word = wordList.removeAt(0)
         }
-
     }
-
-
 }
