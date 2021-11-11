29052e81075610ca216fa902b874f8324870cc97
fadhil27 <fadhilkh27@gmail.com>
2021-11-11 21:19:35 +0800

    Create the GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,14 +1,12 @@
 package com.example.android.guesstheword.screens.game
 
-class GameViewModel {
+class GameViewModel : ViewModel() {
     init {
         Log.i("GameViewModel", "GameViewModel created!")
     }
-
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
     }
-}
 }
